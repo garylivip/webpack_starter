27714f06847e623ebda4aeb2ec7cf9b2f551902c
Gary Li <gary.li@sap.com>
2024-12-30 19:57:56 +0800

    34:17/41:16 works

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # webpack_starter
 webpack starter
+https://webpack.js.org/
 https://www.youtube.com/watch?v=IZGNcSuwBZs
 
 # Dependencies
 -  npm init -y   (-y  skip the questions )
-- npm i -D webpack webpack-cli  (dev dependencies only )
+- npm i -D webpack webpack-cli --registry=https://registry.npmmirror.com (dev dependencies only && local mirror to speed up the installation)
 - npm i -D sass style-loader css-loader sass-loader
+- npm i -D html-webpack-plugin
+- npm install -D webpack-dev-server   =>  Add ("dev": "webpack serve") to package.json, and then npm run dev,  then got below message:
+- - [webpack-cli] For using 'serve' command you need to install: 'webpack-dev-server' package.
+- - [webpack-cli] Would you like to install 'webpack-dev-server' package? (That will run 'npm install -D webpack-dev-server') (Y/n)
+- npm i -D babel-loader @babel/core @babel/preset-env 
 
 # Discriptor
 - "build": "webpack --mode production"  || "build": "webpack" + webpack.config.js  前者优先级高，例如 前者设置 production, .js里面设置 development,  结果输出 process.env.NODE_ENV = production
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { title } = require('process');
 
 module.exports = {
-    mode: 'development', // 'production' or 'development'
+    mode: 'production', // 'production' or 'development'
     // if not specified, the default value is './src', filename is 'index.js'    
     // if not specified, the default value is './dist', default filename is 'main.js'
     // it could single entry point, like above entry: path.resolve(__dirname, 'src', 'index.js'),
     // or multiple entry points
     entry: {
         bundle: path.resolve(__dirname, 'src', 'index.js'),
-        bundle2: path.resolve(__dirname, 'src', 'index.js') // multiple entry points
+        // bundle2: path.resolve(__dirname, 'src', 'index.js') // multiple entry points
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js' // [name] is the key of the entry object or the name of the entry point like 'bundle'
+        filename: '[name][contenthash].js', // [name] is the key of the entry object or the name of the entry point like 'bundle'
+        // contenthash is a hash of the content of the file, so if the content changes, the hash changes
+        clean: true, // Clean the output directory before emit
+        assetModuleFilename: '[name][ext]' 
+    },
+    devtool: 'source-map', // Enable sourcemaps for debugging webpack's output  inline-source-map || source-map
+    devServer:{
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 9000, // Specify the port
+        open: true, // Open the browser
+        hot: true, // Hot Module Replacement
+        compress: true, // Enable gzip compression
+        historyApiFallback: true, // true for index.html upon 404, object for multiple paths
     },
     module: {
         rules: [
@@ -23,7 +39,29 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
+            },
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
+            
         ]
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Webpack 5 Starter',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src', 'template.html'),
+        })
+    ]
 };
